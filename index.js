dfe574da4c334311a665ee19229b6c79be651e66
Gustavo <gordono@unsa.edu.pe>
2022-07-18 10:54:09 -0500

    Added styles to Login form and Google Auth button

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function showLoginView() {
-  console.log("Login View");
+  const main = document.querySelector("#main");
+  main.innerHTML = `
+    <div class="flex flex-col gap-2 items-center">
+      <h2 class="p-2 text-2xl font-bold">Login</h2>
+      <div class="w-7/12 flex flex-col gap-2">
+        <input class="border border-sky-300 p-2" type="text" id="email" placeholder="Enter your Email">
+        <input class="border border-sky-300 p-2" type="password" id="password" placeholder="Enter your Password">
+        <button class="p-2 font-bold text-white bg-blue-600 transition-colors hover:bg-blue-800">Login</button>
+        <a class="p-1 flex gap-2 items-center bg-blue-500 transition-colors hover:bg-blue-600" href="http://localhost:4000/api/auth/google">
+          <img class="w-10 h-10 p-2 bg-white" src="./img/google.svg" alt="Login with Google"></img>
+          <span class="font-bold text-white">Login with Google</span>
+        </a>
+      </div>
+    </div>
+  `;
 }
 
 let socket;
