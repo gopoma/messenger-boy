f3141ceec97bbf6c087d83e0535b7efd81276fb3
Gustavo <gordono@unsa.edu.pe>
2022-07-18 01:39:16 -0500

    Implemented pair messaging functionality

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
-const socket = io.connect("http://localhost:4000", {
-  withCredentials: true
-});
+let socket;
+
+function connectSocket() {
+  socket = io.connect("http://localhost:4000", {
+    withCredentials: true
+  });
+
+  socket.emit("userConnected");
+
+  socket.on("userConnected", activeUsers => {
+    console.log(activeUsers);
+  });
+  
+  socket.on("userDisconnected", activeUsers => {
+    console.log(activeUsers);
+  });
+
+  socket.on("messageReceived", messages => {
+    console.log(messages);
+  });
+  
+  socket.on("messageSended", messages => {
+    console.log(messages);
+  });
+
+  socket.on("messages", messages => {
+    console.log(messages);
+  })
+}
 
 fetch("http://localhost:4000/api/auth/validate", {
   credentials: "include"
 })
-.then(response => response.json())
-.then(console.log)
+.then(response => {
+  if(response.ok) {
+    return response.json();
+  }
+
+  // throw new Error("Not Allowed");
+})
+.then(() => {
+  connectSocket();
+})
 .catch(console.log)
+
+function doLogin() {
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  
+  fetch("http://localhost:4000/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value
+    }),
+    credentials: "include"
+  })
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+
+    // throw new Error("Not Allowed");
+  })
+  .then(() => {
+    connectSocket();
+  })
+  .catch(console.log());
+}
 
 function doLogout() {
   fetch("http://localhost:4000/api/auth/logout", {
@@ -18,33 +80,12 @@ function doLogout() {
   .catch(console.log)
 }
 
-function connect() {
-  const idUser = document.querySelector("#idUser");
-
-  document.querySelector("#currentUserId").innerText = idUser.value;
-  document.querySelector("#currentSocketId").innerText = socket.id;
-  socket.emit("userConnected");
+function beginChat() {
+  const idChat = document.querySelector("#idChat");
+  socket.emit("beginChat", idChat.value);
 }
-
-socket.on("userConnected", activeUsers => {
-  console.log(activeUsers);
-});
-
-socket.on("userDisconnected", activeUsers => {
-  console.log(activeUsers);
-});
 
 function sendMessage() {
-  const idSocket = document.querySelector("#idSocket");
   const message = document.querySelector("#message");
-
-  socket.emit("sendMessage", idSocket.value, message.value);
+  socket.emit("sendMessage", message.value);
 }
-
-socket.on("messageReceived", messages => {
-  console.log(messages);
-});
-
-socket.on("messageSended", messages => {
-  console.log(messages);
-});
